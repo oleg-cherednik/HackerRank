53d6237f3914a206c0be57889323549cf72bfd2f
oleg.cherednik <oleg.cherednik@gmail.com>
2017-09-29 02:34:27 +0300

    sql

--- SQL/Advanced Select/Type of Triangle/request.sql
+++ SQL/Advanced Select/Type of Triangle/request.sql
@@ -2,7 +2,7 @@ select
 case
     when A + B <= C or B + C <= A or A + C <= B then 'Not A Triangle'
     when A = B and B = C and A = C then 'Equilateral'
-	when A = B or B = C or A = C then 'Isosceles'
+    when A = B or B = C or A = C then 'Isosceles'
     else 'Scalene'
 end
 from triangles;
