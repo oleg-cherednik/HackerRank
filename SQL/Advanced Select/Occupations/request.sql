8f6c6ff945ab11908ff96bfc91d39f55cf4ceadf
oleg.cherednik <oleg.cherednik@gmail.com>
2017-09-30 02:26:27 +0300

    sql

--- SQL/Advanced Select/Occupations/request.sql
+++ SQL/Advanced Select/Occupations/request.sql
@@ -1,9 +1,18 @@
-select val
+set @d=0, @p=0, @s=0, @a=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
 from (
-    select 'a' k, concat(name, '(', left(occupation, 1), ')') val from occupations
-    union
-    select 'b' k, concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') total
+    select
+        case
+            when occupation = 'Doctor' then (@d:=@d+1)
+            when occupation = 'Professor' then (@p:=@p+1)
+            when occupation = 'Singer' then (@s:=@s+1)
+            when occupation = 'Actor' then (@a:=@a+1)
+        end as id,
+        case when occupation = 'Doctor' then name end as Doctor,
+        case when occupation = 'Professor' then name end as Professor,
+        case when occupation = 'Singer' then name end as Singer,
+        case when occupation = 'Actor' then name end as Actor
     from occupations
-    group by occupation
-) as a
-order by k asc, val asc
+    order by name
+) tmp
+group by id
