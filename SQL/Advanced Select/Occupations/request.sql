14a8adbba4809b456054877e7736423239e47520
oleg.cherednik <oleg.cherednik@gmail.com>
2017-09-30 00:10:51 +0300

    sql

---
+++ SQL/Advanced Select/Occupations/request.sql
@@ -0,0 +1,9 @@
+select val
+from (
+    select 'a' k, concat(name, '(', left(occupation, 1), ')') val from occupations
+    union
+    select 'b' k, concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') total
+    from occupations
+    group by occupation
+) as a
+order by k asc, val asc
