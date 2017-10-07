ce59301ad64f274583777e908cb788805a209823
oleg.cherednik <oleg.cherednik@gmail.com>
2017-10-07 21:42:33 +0300

    RegEx

---
+++ Regex/Repetitions/Matching {x, y} Repetitions/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Oleg Cherednik
+ * @since 06.10.2017
+ */
+public class Solution {
+    public static void main(String... args) {
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[A-Za-z02468]{40}[\\s13579]{5}$");
+    }
+}
+
+class Regex_Test {
+    public void checker(String Regex_Pattern) {
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }
+}
