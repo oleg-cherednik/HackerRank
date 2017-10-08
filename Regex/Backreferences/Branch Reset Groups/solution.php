9fb9c1e4e36b70b785064f4c1bbc418ef8942b0a
oleg.cherednik <oleg.cherednik@gmail.com>
2017-10-08 21:10:52 +0300

    RegEx

---
+++ Regex/Backreferences/Branch Reset Groups/solution.php
@@ -0,0 +1,11 @@
+<?php
+$Regex_Pattern = '/^\d{2}(?|(-)|(---)|(\.)|(\:))\d{2}\1\d{2}\1\d{2}$/';
+$handle = fopen("php://stdin","r");
+$Test_String = fgets($handle);
+if(preg_match($Regex_Pattern, $Test_String, $output_array)) {
+	print("true");
+} else {
+	print("false");
+}
+fclose($handle);
+?>
