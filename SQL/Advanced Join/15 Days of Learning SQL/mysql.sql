ab75f6fe546610b7b1be5fc797e4b818efa44e0f
oleg.cherednik <oleg.cherednik@gmail.com>
2017-10-25 21:57:04 +0300

    15 Days of Learning SQL

--- SQL/Advanced Join/15 Days of Learning SQL/mysql.sql
+++ SQL/Advanced Join/15 Days of Learning SQL/mysql.sql
@@ -21,4 +21,20 @@ from (
 		group by t3.submission_date) t4) t5
 join hackers h on h.hacker_id = t5.hacker_id
 order by submission_date
-        
+
+select
+	submission_date,
+	(select count(distinct hacker_id)
+     from submissions s2
+	 where s2.submission_date = s1.submission_date and (select count(distinct s3.submission_date)
+														from submissions s3
+                                                        where s3.hacker_id = s2.hacker_id and s3.submission_date < s1.submission_date) = datediff(s1.submission_date, '2016-03-01')) total,
+	(select hacker_id
+     from submissions s2
+     where s2.submission_date = s1.submission_date 
+	 group by hacker_id
+     order by count(submission_id) desc, hacker_id
+     limit 1) shit,
+	(select name from hackers where hacker_id = shit)
+from (select distinct submission_date from submissions) s1
+group by submission_date        
