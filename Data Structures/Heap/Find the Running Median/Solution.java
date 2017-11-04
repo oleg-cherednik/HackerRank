becb43c52ffb80e8008abf559d61f6c2928f6c21
oleg.cherednik <oleg.cherednik@gmail.com>
2017-11-05 01:35:25 +0300

    Find the Running Median

--- Data Structures/Heap/Find the Running Median/Solution.java
+++ Data Structures/Heap/Find the Running Median/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
@@ -9,28 +10,37 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int K = scan.nextInt();
+        int n = scan.nextInt();
         scan.nextLine();
 
-        Queue<Integer> queue = new PriorityQueue<>();
+        Queue<Integer> heap1 = new PriorityQueue<>((val1, val2) -> -Integer.compare(val1, val2));
+        Queue<Integer> heap2 = new PriorityQueue<>();
 
-        for (int i = 0; i < N; i++)
-            queue.add(scan.nextInt());
+        for (int i = 0; i < n; i++) {
+            int a = scan.nextInt();
 
-        scan.close();
-        int res = 0;
+            if (heap1.isEmpty())
+                heap1.add(a);
+            else if (heap1.element() > a) {
+                heap2.add(heap1.remove());
+                heap1.add(a);
+            } else
+                heap2.add(a);
 
-        while (queue.size() > 1 && queue.peek() < K) {
-            int a = queue.poll();
-            int b = queue.poll();
+            if (heap1.size() > heap2.size() + 1)
+                heap2.add(heap1.remove());
+            else if (heap2.size() > heap1.size() + 1)
+                heap1.add(heap2.remove());
 
-            queue.add(a + 2 * b);
-            res++;
+            if (heap1.size() == heap2.size())
+                System.out.format(Locale.US, "%.1f\n", (heap1.element() + heap2.element()) / 2.);
+            else if (heap1.size() > heap2.size())
+                System.out.format(Locale.US, "%.1f\n", (double)heap1.element());
+            else
+                System.out.format(Locale.US, "%.1f\n", (double)heap2.element());
         }
 
-        System.out.print(queue.peek() < K ? -1 : res);
+        scan.close();
     }
-
 }
 
