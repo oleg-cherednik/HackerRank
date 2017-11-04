706882ffebc0b89053d910cedd7c0536ef1a3db7
oleg.cherednik <oleg.cherednik@gmail.com>
2017-11-05 01:01:58 +0300

    Find the Running Median

---
+++ Data Structures/Heap/Find the Running Median/Solution.java
@@ -0,0 +1,36 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 04.11.2017
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+        scan.nextLine();
+
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++)
+            queue.add(scan.nextInt());
+
+        scan.close();
+        int res = 0;
+
+        while (queue.size() > 1 && queue.peek() < K) {
+            int a = queue.poll();
+            int b = queue.poll();
+
+            queue.add(a + 2 * b);
+            res++;
+        }
+
+        System.out.print(queue.peek() < K ? -1 : res);
+    }
+
+}
+
