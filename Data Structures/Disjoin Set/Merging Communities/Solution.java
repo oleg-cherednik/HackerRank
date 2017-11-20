b9dce5788ed31fb14b68c2fa18dea0ec119cdbe6
oleg.cherednik <oleg.cherednik@gmail.com>
2017-11-20 12:42:21 +0300

    Merging Communities

---
+++ Data Structures/Disjoin Set/Merging Communities/Solution.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.11.2017
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int N = scan.nextInt();
+            int Q = scan.nextInt();
+            scan.nextLine();
+
+            UnionFind graph = new UnionFind(N + 1);
+
+            for (int i = 0; i < Q; i++) {
+                String cmd = scan.next();
+
+                if ("Q".equals(cmd))
+                    System.out.println(graph.total(scan.nextInt()));
+                else if ("M".equals(cmd))
+                    graph.union(scan.nextInt(), scan.nextInt());
+            }
+        }
+    }
+
+    private static class UnionFind {
+        private final int[] id;
+        private final int[] sz;
+
+        public UnionFind(int N) {
+            id = new int[N];
+            sz = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                id[i] = i;
+                sz[i] = 1;
+            }
+        }
+
+        public int total(int p) {
+            return sz[find(p)];
+        }
+
+        private int find(int p) {
+            while (p != id[p])
+                p = id[p];
+            return p;
+        }
+
+        public void union(int p, int q) {
+            int i = find(p);
+            int j = find(q);
+
+            if (i == j)
+                return;
+
+            if (sz[i] < sz[j]) {
+                id[i] = j;
+                sz[j] += sz[i];
+                sz[i] = sz[j];
+            } else {
+                id[j] = i;
+                sz[i] += sz[j];
+                sz[j] = sz[i];
+            }
+        }
+    }
+}
