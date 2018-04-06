2789ee242e56f6cc134134b87150e26c32eb30c3
oleg.cherednik <oleg.cherednik@gmail.com>
2018-04-06 08:20:13 +0300

    fix inspections

--- Data Structures/Arrays/Arrays - DS/Solution.java
+++ Data Structures/Arrays/Arrays - DS/Solution.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
  * @since 26.10.2017
  */
 public class Solution {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
         for (int arr_i = 0; arr_i < n; arr_i++)
             arr[arr_i] = in.nextInt();
