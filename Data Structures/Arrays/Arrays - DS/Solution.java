cc4ee462e0550fc67e874df7254edf0a380b0778
oleg.cherednik <oleg.cherednik@gmail.com>
2018-04-06 08:03:43 +0300

    remove brackets

--- Data Structures/Arrays/Arrays - DS/Solution.java
+++ Data Structures/Arrays/Arrays - DS/Solution.java
@@ -10,9 +10,8 @@ public class Solution {
         int n = in.nextInt();
         int arr[] = new int[n];
 
-        for (int arr_i = 0; arr_i < n; arr_i++) {
+        for (int arr_i = 0; arr_i < n; arr_i++)
             arr[arr_i] = in.nextInt();
-        }
 
         for (int i = arr.length - 1; i >= 0; i--)
             System.out.print(arr[i] + " ");
