dc0e11cc5044e1cdd1c2ff33e057c50d71325b99
oleg.cherednik <oleg.cherednik@gmail.com>
2018-04-29 20:36:26 +0300

    Day 2: Loops

---
+++ Tutorials/10 Days of JavaScript/Day 2.3 - Loops/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for(let i = 0; i < s.length; i++) {
+        switch(s[i]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                console.log(s[i]);
+        }
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        switch(s[i]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                break;
+            default:
+                console.log(s[i]);
+        }
+    }
+}
+
+function main() {
+    const s = readLine();
+
+    console.log(vowelsAndConsonants(s));
+}
