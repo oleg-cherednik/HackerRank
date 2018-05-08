86b26ea9cd38b1b4267bddfce23372b0826b0ad6
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-08 10:40:31 +0300

    Day 7: Regular Expressions III

---
+++ Tutorials/10 Days of JavaScript/Day 7.3 - Regular Expressions III/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+
+    /*
+     * Do not remove the return statement
+     */
+    return /\d+/g;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+
+    console.log(re.test(s));
+}
