fcca297e6b11deb783a41a1f1d7440803a008550
Oleg Cherednik <Oleg_Cherednik@epam.com>
2018-05-18 12:03:27 +0300

    Day 6: Let's Review

---
+++ Tutorials/30 Days of Code/Day 6 - Let's Review/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 18.05.2018
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        try (Scanner scan = new Scanner(System.in)) {
+            int T = scan.nextInt();
+
+            scan.nextLine();
+
+            for (int i = 0; i < T; i++) {
+                String str = scan.nextLine();
+                StringBuilder even = new StringBuilder(str.length());
+                StringBuilder odd = new StringBuilder(str.length());
+
+                for (int j = 0; j < str.length(); j++) {
+                    (j % 2 == 0 ? even : odd).append(str.charAt(j));
+                }
+
+                System.out.println(even + " " + odd);
+            }
+        }
+    }
+}
