edb195172aab39906aa8914cd3fa3443e4b1c60e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-20 20:41:11 +0300

    Time Complexity: Primality

---
+++ Tutorials/Cracking the Coding Interview/Techniques and Concepts/Time Complexity - Primality/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.05.2018
+ */
+public class Solution {
+
+private static boolean isPrime(int n) {
+    if (n < 2)
+        return false;
+    for (int i = 2, max = (int)Math.sqrt(n); i <= max; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int pItr = 0; pItr < p; pItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
+        }
+
+        scanner.close();
+    }
+
+}
