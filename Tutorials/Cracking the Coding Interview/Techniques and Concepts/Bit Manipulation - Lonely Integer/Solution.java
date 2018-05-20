be61db9345870bfa3cbfedc0500103c28b5d98bb
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-20 20:48:48 +0300

    Bit Manipulation: Lonely Integer

---
+++ Tutorials/Cracking the Coding Interview/Techniques and Concepts/Bit Manipulation - Lonely Integer/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.05.2018
+ */
+public class Solution {
+
+    public static int lonelyInteger(int[] arr) {
+        int value = 0;
+
+        for (int i : arr)
+            value ^= i;
+
+        return value;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        System.out.println(lonelyInteger(a));
+
+        scanner.close();
+    }
+}
