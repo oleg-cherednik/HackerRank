68fa17bcdca326c2983cd9c7102e12469c5b7ed9
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-20 16:11:09 +0300

    Day 7: Arrays

---
+++ Tutorials/30 Days of Code/Day 7 - Arrays/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.05.2018
+ */
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+
+        scanner.close();
+    }
+}
