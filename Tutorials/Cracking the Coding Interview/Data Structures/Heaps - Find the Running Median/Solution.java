b39440273705ebb3921ab24f6a51a3bb2996eac6
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-20 19:04:43 +0300

    Heaps: Find the Running Median

--- Tutorials/Cracking the Coding Interview/Data Structures/Heaps - Find the Running Median/Solution.java
+++ Tutorials/Cracking the Coding Interview/Data Structures/Heaps - Find the Running Median/Solution.java
@@ -1,3 +1,6 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -6,119 +9,43 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    private static class Tree {
+    private static final class MedianHeap {
 
-        private Node root;
+        private final Queue<Integer> max;
+        private final Queue<Integer> min;
+
+        public MedianHeap(int size) {
+            final Comparator<Integer> comparator = Integer::compare;
+            max = new PriorityQueue<>(size, comparator.reversed());
+            min = new PriorityQueue<>(size, comparator);
+        }
 
         public void add(int val) {
-            if (root == null)
-                root = new Node(val);
-            else {
-                add(root, val, false);
+            if (!min.isEmpty() && val > min.element()) {
+                min.add(val);
 
-                if (root.leftSize() < root.rightSize())
-                    leftRotate();
-                else if (root.leftSize() - root.rightSize() > 1)
-                    rightRotate();
+                while (min.size() - max.size() > 0) {
+                    max.add(min.remove());
+                }
+            } else {
+                max.add(val);
+
+                while (max.size() - min.size() > 1) {
+                    min.add(max.remove());
+                }
             }
-
-//            System.out.println(median());
-        }
-
-        private void add(Node node, int val, boolean rightSide) {
-            if (val < node.data) {
-                if (node.left != null) {
-                    add(node.left, val, node != root && rightSide);
-                    node.checkLeft();
-                } else
-                    node.left = new Node(val);
-            } else if (node.right != null) {
-                add(node.right, val, node == root || rightSide);
-                node.checkRight();
-            } else
-                node.right = new Node(val);
-        }
-
-        private void leftRotate() {
-            Node prvRoot = root;
-            root = prvRoot.right;
-            prvRoot.right = null;
-            root.left = prvRoot;
-        }
-
-        private void rightRotate() {
-            Node prvRoot = root;
-            root = prvRoot.left;
-            prvRoot.left = null;
-            root.right = prvRoot;
         }
 
         public double median() {
-            if (root == null)
+            if (max.isEmpty())
                 return Double.NaN;
-            return root.size() % 2 == 0 ? (root.left.data + root.data) / 2.0 : (double)root.data;
-        }
-
-        private static final class Node {
-
-            private final int data;
-            private Node left;
-            private Node right;
-
-            public Node(int data) {
-                this.data = data;
-            }
-
-            public int size() {
-                return leftSize() + rightSize() + 1;
-            }
-
-            public int leftSize() {
-                return left != null ? left.size() : 0;
-            }
-
-            public int rightSize() {
-                return right != null ? right.size() : 0;
-            }
-
-            private void checkLeft() {
-                if (left.right != null) {
-                    Node tmp = left.right;
-                    left.right.left = left;
-                    left.right = null;
-                    left = tmp;
-                }
-            }
-
-            private void checkRight() {
-                if (right.left != null) {
-                    Node tmp = right.left;
-                    right.left.right = right;
-                    right.left = null;
-                    right = tmp;
-                }
-            }
+            return max.size() == min.size() ? (max.element() + min.element()) / 2. : (double)max.element();
         }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        Tree tree = new Tree();
-//        tree.add(1);
-//        tree.add(3);
-//        tree.add(5);
-//        tree.add(7);
-//        tree.add(9);
-//
-//        tree.add(4);
-//        tree.add(2);
-//        tree.add(6);
-//        tree.add(8);
-//
-//        int a = 0;
-//        a++;
-
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
