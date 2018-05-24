9027715a7fbb0be52c8eedf39aef39f1b31b964b
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-24 04:11:47 +0300

    Tries: Contacts

--- Tutorials/Cracking the Coding Interview/Data Structures/Tries - Contacts/Solution.java
+++ Tutorials/Cracking the Coding Interview/Data Structures/Tries - Contacts/Solution.java
@@ -10,35 +10,11 @@ public class Solution {
 
     private static final class SuffixTree {
 
-        private final Node root = new Node('\0');
+        private final Node root = new Node(null);
 
         public void add(String contact) {
-//            add(contact.toLowerCase(), 0, root);
             root.add(contact);
         }
-
-//        private static void add(String contact, int pos, Node node) {
-//            String common = node.common(contact);
-//
-//            if(common == null) {
-//                if(node.str == null)
-//                    node.str = contact;
-//                else {
-//
-//                }
-//
-//            } else {
-//
-//            }
-//
-//
-//            if (pos < contact.length())
-//                add(contact, pos + 1, node.getOrCreate(contact.charAt(pos)));
-//            else
-//                node.last = true;
-
-
-//        }
 
         public int find(String contact) {
             Node node = find(contact, 0, root);
@@ -62,15 +38,51 @@ public class Solution {
             return total;
         }
 
+        /*
+        9
+add hack
+add hackerrank
+add hackaton
+add harmony
+add harriet
+add marriet
+add marvel
+find hac
+find hak
+
+         */
+
         private static Node find(String contact, int pos, Node node) {
-            while (true) {
-                if (node == null)
-                    return null;
-                if (pos == contact.length())
-                    return node;
-                node = node.get(contact.charAt(pos));
-                pos++;
+            if (node.str == null)
+                return find(contact, pos, node.map.get(contact.charAt(pos)));
+
+            String str = contact.substring(pos);
+
+            if(str.equals(node.str))
+                return node;
+            if(str.startsWith(node.str)) {
+                char ch = str.substring(node.str.length()).charAt(0);
+                Node next = find(contact, node.str.length() + pos, node.map.get(ch));
+                return next != null ? next : node;
             }
+
+            return node;
+
+
+
+//            while (true) {
+//                if (node == null)
+//                    return null;
+//                if (pos == contact.length())
+//                    return node;
+//                if(node.str == null)
+//                    node = node.map.get(contact.charAt(pos));
+//                else if(contact.substring(pos).startsWith(node.str))
+//                    node = node.map
+//
+//                    pos++;
+//                }
+//            }
         }
 
         private static final class Node {
@@ -78,79 +90,63 @@ public class Solution {
             private final Map<Character, Node> map = new HashMap<>();
 
             private String str;
-            private char ch;
             private boolean last;
 
             public Node(String str) {
                 this.str = str;
             }
 
-            public Node(char ch) {
-                this.ch = ch;
-            }
-
-            public boolean isExists(char ch) {
-                return map.containsKey(ch);
-            }
-
-            public Node get(char ch) {
-                return map.get(ch);
-            }
-
-            public Node getOrCreate(char ch) {
-                if (!isExists(ch))
-                    map.put(ch, new Node(ch));
-                return get(ch);
+            public Node(int len, Node node) {
+                str = node.str.substring(len);
+                map.putAll(node.map);
+                last = node.last;
             }
 
             public Node add(String str) {
-                if (str.equals(this.str)) {
+                Node res = this;
+
+                if (this.str == null)
+                    res = addChildNode(str);
+                else if (this.str.equals(str))
                     last = true;
-                    return this;
+                else if (str.startsWith(this.str))
+                    res = addChildNode(str.substring(this.str.length()));
+                else {
+                    String common = common(str);
+                    Node node = new Node(common.length(), this);
+
+                    this.str = common;
+                    last = false;
+                    map.clear();
+                    map.put(node.str.charAt(0), node);
+
+                    res = add(str);
                 }
 
-                String common = common(str);
-
-                if (common == null) {
-                    if (this.str == null) {
-                        this.str = str;
-                        this.last = true;
-                    } else {
-                        int a = 0;
-                        a++;
-                    }
-                } else {
-                    String suffix = str.substring(common.length());
-
-                    if (this.str.equals(common)) {
-                        if(map.containsKey(suffix.charAt(0)))
-                            map.get(suffix.charAt(0)).add(suffix.substring(0));
-                        else
-                            map.put(suffix.charAt(0), new Node(suffix.substring(0)));
-                    } else {
-                        int a = 0;
-                        a++;
-                    }
-
-                }
-
-                return null;
+                return res;
             }
 
-            public String common(String str) {
-                if (this.str == null)
-                    return null;
+            private Node addChildNode(String str) {
+                char ch = str.charAt(0);
+                Node node = map.get(ch);
 
+                if (node == null)
+                    map.put(ch, node = new Node(str));
+
+                return node.add(str);
+            }
+
+            private String common(String str) {
                 for (int i = 0, min = Math.min(str.length(), this.str.length()); i < min; i++)
                     if (str.charAt(i) != this.str.charAt(i))
-                        return i == 0 ? null : str.substring(0, i);
+                        return i == 0 ? "" : str.substring(0, i);
 
                 return str.length() < this.str.length() ? str : this.str;
             }
 
             @Override
             public String toString() {
-                return String.valueOf(ch);
+                return str + (last ? "$" : "");
             }
         }
     }
