81c88f193653a0dd206ec0909be17040802bcfb2
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-24 18:19:50 +0300

    Hash Tables: Ice Cream Parlor

---
+++ Tutorials/Cracking the Coding Interview/Algorithms/Hash Tables - Ice Cream Parlor/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 24.05.2018
+ */
+public class Solution {
+
+    static void solve(int[] arr, int money) {
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            solve(arr, money);
+        }
+
+        scanner.close();
+    }
+
+}
