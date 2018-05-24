bfe584750774b488c8f99ed2c2380e63f81e6a6e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-24 19:01:13 +0300

    Hash Tables: Ice Cream Parlor

--- Tutorials/Cracking the Coding Interview/Algorithms/Hash Tables - Ice Cream Parlor/Solution.java
+++ Tutorials/Cracking the Coding Interview/Algorithms/Hash Tables - Ice Cream Parlor/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,7 +9,19 @@ import java.util.Scanner;
 public class Solution {
 
     static void solve(int[] arr, int money) {
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= money)
+                continue;
+
+            if (map.containsKey(money - arr[i])) {
+                System.out.println(map.get(money - arr[i]) + " " + (i + 1));
+                break;
+            }
+
+            map.put(arr[i], i + 1);
+        }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -26,7 +40,7 @@ public class Solution {
             int[] arr = new int[n];
 
             String[] arrItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
             for (int i = 0; i < n; i++) {
                 int arrItem = Integer.parseInt(arrItems[i]);
