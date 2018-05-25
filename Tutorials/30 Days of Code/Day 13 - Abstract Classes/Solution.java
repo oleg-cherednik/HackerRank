44fe44cb36d9f19a6c2f03bd792cc38be4f132d4
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-25 09:17:24 +0300

    Day 13: Abstract Classes

---
+++ Tutorials/30 Days of Code/Day 13 - Abstract Classes/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 24.05.2018
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
+
+abstract class Book {
+
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+
+    int price;
+
+    /**
+     * Class Constructor
+     *
+     * @param title  The book's title.
+     * @param author The book's author.
+     * @param price  The book's price.
+     **/
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    /**
+     * Method Name: display
+     *
+     * Print the title, author, and price in the specified format.
+     **/
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
