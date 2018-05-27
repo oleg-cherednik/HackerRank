4420fab6609933e7aa71247797e22552045cd389
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-27 06:55:52 +0300

    Solve Me First

---
+++ Algorithms/Warmup/Solve Me First/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 27.05.2018
+ */
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
