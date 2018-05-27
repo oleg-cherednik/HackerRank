8eee22891fe79cf301c9c3e54f45777f1bcad8d8
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-27 08:30:28 +0300

    Apple and Orange

---
+++ Algorithms/Implementation/Apple and Orange/Solution.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 27.05.2018
+ */
+public class Solution {
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int apple = 0;
+        int orange = 0;
+
+        for (int d : apples)
+            if (s - a <= d)
+                apple++;
+
+        for (int d : oranges)
+            if (d < 0 && b - t <= Math.abs(d))
+                orange++;
+
+        System.out.println(apple);
+        System.out.println(orange);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+}
