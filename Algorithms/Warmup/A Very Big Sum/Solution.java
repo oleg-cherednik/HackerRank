28d1e30986500487ea701be54d25d732f3479704
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-27 07:33:27 +0300

    A Very Big Sum

--- Algorithms/Warmup/A Very Big Sum/Solution.java
+++ Algorithms/Warmup/A Very Big Sum/Solution.java
@@ -16,10 +16,12 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         long[] ar = new long[arCount];
 
         String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < arCount; i++) {
             long arItem = Long.parseLong(arItems[i]);
