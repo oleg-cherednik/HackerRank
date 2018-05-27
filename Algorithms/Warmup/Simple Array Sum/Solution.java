52caaf8a3b042ea71c6a23efa6072cd2e9a460cd
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-27 06:59:00 +0300

    Simple Array Sum

---
+++ Algorithms/Warmup/Simple Array Sum/Solution.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 27.05.2018
+ */
+public class Solution {
+
+    static int simpleArraySum(int[] ar) {
+        return Arrays.stream(ar).sum();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(scanner.nextLine().trim());
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+
+        for (int arItr = 0; arItr < arCount; arItr++) {
+            int arItem = Integer.parseInt(arItems[arItr].trim());
+            ar[arItr] = arItem;
+        }
+
+        int result = simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
