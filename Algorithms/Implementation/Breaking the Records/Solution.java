7e5309a946fceb4d370aba318a77d6feea9abb96
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-05-28 09:09:47 +0300

    Breaking the Records

---
+++ Algorithms/Implementation/Breaking the Records/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 28.05.2018
+ */
+public class Solution {
+
+    static int[] breakingRecords(int[] score) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] score = new int[n];
+
+        String[] scoreItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoreItem = Integer.parseInt(scoreItems[i]);
+            score[i] = scoreItem;
+        }
+
+        int[] result = breakingRecords(score);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
