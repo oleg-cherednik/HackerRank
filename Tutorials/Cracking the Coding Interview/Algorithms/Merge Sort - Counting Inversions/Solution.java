eba576605136d1e7d517744cfdf045f8bb59fc28
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-06-01 23:22:29 +0300

    Merge Sort: Counting Inversions

---
+++ Tutorials/Cracking the Coding Interview/Algorithms/Merge Sort - Counting Inversions/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 01.06.2018
+ */
+public class Solution {
+
+    static long countInversions(int[] arr) {
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            long result = countInversions(arr);
+
+            System.out.println(String.valueOf(result));
+        }
+
+        scanner.close();
+    }
+
+}
