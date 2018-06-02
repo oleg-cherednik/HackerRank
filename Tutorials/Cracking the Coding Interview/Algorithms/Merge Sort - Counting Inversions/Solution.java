d56950381f46aaa6314795cf98d69987b699a3a9
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-06-02 09:56:17 +0300

    Merge Sort: Counting Inversions

--- Tutorials/Cracking the Coding Interview/Algorithms/Merge Sort - Counting Inversions/Solution.java
+++ Tutorials/Cracking the Coding Interview/Algorithms/Merge Sort - Counting Inversions/Solution.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * @author Oleg Cherednik
@@ -7,7 +9,25 @@ import java.util.Scanner;
 public class Solution {
 
     static long countInversions(int[] arr) {
-        return 0;
+        int res = 0;
+
+        Set<Integer> values = new HashSet<>();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(values.contains(arr[i]))
+                continue;
+
+            for(int j = arr.length - 1; j >i; j--) {
+                if (arr[j] <= arr[i]) {
+                    res = Math.max(res, j - i);
+                    break;
+                }
+            }
+
+            values.add(arr[i]);
+        }
+
+        return res;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
