b768adb7707198425fda3936dfb1f5d6ffed731e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-06-05 06:05:55 +0300

    Day 23: BST Level-Order Traversal

---
+++ Tutorials/30 Days of Code/Day 23 - BST Level-Order Traversal/Solution.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 05.06.2018
+ */
+public class Solution {
+
+    static void levelOrder(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            System.out.print(node.data + " ");
+
+            if (node.left != null)
+                queue.add(node.left);
+            if (node.right != null)
+                queue.add(node.right);
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+            T--;
+        }
+        levelOrder(root);
+    }
+}
+
+class Node {
+
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
