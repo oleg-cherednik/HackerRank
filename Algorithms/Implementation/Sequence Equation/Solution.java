310811687c0e9717b8b884cb9057524ab6d20b66
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-06-05 11:14:20 +0300

    Sequence Equation

---
+++ Algorithms/Implementation/Sequence Equation/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 03.06.2018
+ */
+public class Solution {
+
+    static int[] permutationEquation(int[] p) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                System.out.println();
+            }
+        }
+
+        scanner.close();
+    }
+}
