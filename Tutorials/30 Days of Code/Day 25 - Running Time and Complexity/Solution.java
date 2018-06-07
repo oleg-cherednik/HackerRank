a60f9610fd6e8f8bc87152a4a06fda4bd576097e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-06-07 10:12:55 +0300

    Day 25: Running Time and Complexity

---
+++ Tutorials/30 Days of Code/Day 25 - Running Time and Complexity/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 07.06.2018
+ */
+public class Solution {
+
+    private static boolean isPrime(int val) {
+        if (val < 2)
+            return false;
+
+        int sqrt = (int)Math.sqrt(val);
+
+        for (int i = 2; i <= sqrt; i++)
+            if (val % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int T = scan.nextInt();
+
+            for (int i = 0; i < T; i++)
+                System.out.println(isPrime(scan.nextInt()) ? "Prime" : "Not prime");
+        }
+    }
+
+}
+
