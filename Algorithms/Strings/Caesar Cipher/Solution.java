9dd9db67f5f7d01033488842538ff97caa5bdd2a
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-13 11:46:52 +0300

    Caesar Cipher

---
+++ Algorithms/Strings/Caesar Cipher/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 13.07.2018
+ */
+public class Solution {
+
+    static String caesarCipher(String s, int k) {
+        k %= 26;
+        StringBuilder buf = new StringBuilder(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isAlphabetic(ch)) {
+                boolean upperCase = Character.isUpperCase(ch);
+                ch = (char)(Character.toLowerCase(s.charAt(i)) + k);
+                ch = ch > 'z' ? (char)(ch - 'z' + 'a' - 1) : ch;
+                ch = upperCase ? Character.toUpperCase(ch) : ch;
+            }
+
+            buf.append(ch);
+        }
+
+        return buf.toString();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = caesarCipher(s, k);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
