298ca7eacdd82706e07964016804f90b40fdd15c
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-16 02:38:23 +0300

    Big Sorting

---
+++ Algorithms/Sorting/Big Sorting/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 16.07.2018
+ */
+public class Solution {
+
+    static String[] bigSorting(String[] unsorted) {
+        Arrays.sort(unsorted, (x, y) -> x.length() == y.length() ? x.compareTo(y) : Integer.compare(x.length(), y.length()));
+        return unsorted;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] unsorted = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String unsortedItem = scanner.next();   // nextLine() is too long
+            unsorted[i] = unsortedItem;
+        }
+
+        String[] result = bigSorting(unsorted);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+
+            if (i != result.length - 1) {
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
