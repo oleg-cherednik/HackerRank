b86be08f7d57acad530f35408024b61fb9a0d913
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-16 00:30:26 +0300

    Zig Zag Sequence

--- Algorithms/Debugging/Zig Zag Sequence/Solution.java
+++ Algorithms/Debugging/Zig Zag Sequence/Solution.java
@@ -22,19 +22,19 @@ public class Solution {
 
     public static void findZigZagSequence(int [] a, int n){
         Arrays.sort(a);
-        int mid = (n + 1)/2;
+        int mid = (n - 1)/2;
         int temp = a[mid];
         a[mid] = a[n - 1];
         a[n - 1] = temp;
 
         int st = mid + 1;
-        int ed = n - 1;
+        int ed = n - 2;
         while(st <= ed){
             temp = a[st];
             a[st] = a[ed];
             a[ed] = temp;
             st = st + 1;
-            ed = ed + 1;
+            ed = ed - 1;
         }
         for(int i = 0; i < n; i++){
             if(i > 0) System.out.print(" ");
