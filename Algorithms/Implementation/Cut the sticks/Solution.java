18efea7583de80c5cb453e9670b41e1905b8feeb
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-17 18:18:09 +0300

    Cut the sticks

---
+++ Algorithms/Implementation/Cut the sticks/Solution.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 17.17.2018
+ */
+public class Solution {
+
+    static int[] cutTheSticks(int[] arr) {
+        Arrays.sort(arr);
+
+        List<Integer> tmp = new ArrayList<>();
+
+        for (int i = 0, k = 0; i < arr.length; i++, k = 0) {
+            if (arr[i] == 0)
+                continue;
+
+            int sub = arr[i];
+
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] != 0) {
+                    k++;
+                    arr[j] -= sub;
+                }
+            }
+
+            tmp.add(k);
+        }
+
+        return tmp.stream().mapToInt(i -> i).toArray();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = cutTheSticks(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
