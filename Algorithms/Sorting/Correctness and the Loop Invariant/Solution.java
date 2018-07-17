1db042eef84c1c2ce1d05ff9d89ed1ad882a7003
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-17 08:10:07 +0300

    Correctness and the Loop Invariant

--- Algorithms/Sorting/Correctness and the Loop Invariant/Solution.java
+++ Algorithms/Sorting/Correctness and the Loop Invariant/Solution.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    public static void insertionSort(int[] A){
-        for(int i = 1; i < A.length; i++){
+    public static void insertionSort(int[] A) {
+        for (int i = 1; i < A.length; i++) {
             int value = A[i];
             int j = i - 1;
-            while(j > 0 && A[j] > value){
+            while (j >= 0 && A[j] > value) {
                 A[j + 1] = A[j];
                 j = j - 1;
             }
@@ -20,18 +20,18 @@ public class Solution {
         printArray(A);
     }
 
-
     static void printArray(int[] ar) {
-        for(int n: ar){
-            System.out.print(n+" ");
+        for (int n : ar) {
+            System.out.print(n + " ");
         }
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] ar = new int[n];
-        for(int i=0;i<n;i++){
-            ar[i]=in.nextInt();
+        for (int i = 0; i < n; i++) {
+            ar[i] = in.nextInt();
         }
         insertionSort(ar);
     }
