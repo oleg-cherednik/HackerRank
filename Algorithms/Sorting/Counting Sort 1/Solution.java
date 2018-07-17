b5ef39b04e60a5bf4f07d7cd3edc520d9860637f
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-17 08:17:51 +0300

    Counting Sort 1

---
+++ Algorithms/Sorting/Counting Sort 1/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 17.07.2018
+ */
+public class Solution {
+
+    static int[] countingSort(int[] arr) {
+        int[] res = new int[100];
+
+        for (int i = 0; i < arr.length; i++)
+            res[arr[i]]++;
+
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
