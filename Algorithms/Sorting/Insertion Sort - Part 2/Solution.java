676ccf49c61ac45bfe6b885f02e6a3bacf2b20aa
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-17 08:49:55 +0300

    Insertion Sort - Part 2

--- Algorithms/Sorting/Insertion Sort - Part 2/Solution.java
+++ Algorithms/Sorting/Insertion Sort - Part 2/Solution.java
@@ -11,18 +11,12 @@ public class Solution {
         for (int i = 1, j = i - 1; i < arr.length; i++, j = i - 1) {
             int x = arr[i];
 
-            while (true) {
-                if (j < 0 || arr[j] < x) {
-                    if (j + 1 != i) {
-                        System.arraycopy(arr, j + 1, arr, j + 2, i - j - 1);
-                        arr[j + 1] = x;
-                    }
-                    break;
-                }
-
-                j--;
+            while (j >= 0 && arr[j] > x) {
+                arr[j + 1] = arr[j];
+                j -= 1;
             }
 
+            arr[j + 1] = x;
             PRINT.accept(arr);
         }
     }
