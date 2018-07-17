a3929cc48c0a6071fe98523e626200aa8206ea9d
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-17 12:47:31 +0300

    Lily's Homework

---
+++ Algorithms/Sorting/Lily's Homework/Solution.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 17.07.2018
+ */
+public class Solution {
+
+    static int lilysHomework(int[] arr) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = lilysHomework(arr);
+        System.out.println(String.valueOf(result));
+
+        scanner.close();
+    }
+}
