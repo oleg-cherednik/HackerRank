c78dfccaa15d89fa3ac33b988df9eaa1982cd82f
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-19 22:14:15 +0300

    Project Euler #4: Largest palindrome product

---
+++ Project Euler/#4 - Largest palindrome product/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Oleg Cherednik
+ * @since 19.07.2018
+ */
+public class Solution {
+
+    private static final Set<Integer> PALINDROMES = new TreeSet<>(Comparator.reverseOrder());
+
+    static {
+        for (int i = 100; i < 1000; i++)
+            for (int j = i; j < 1000; j++)
+                if (isPalindrome(i * j))
+                    PALINDROMES.add(i * j);
+    }
+
+    static int findLargestPalindromeProduct(int n) {
+        for (int palindrome : PALINDROMES)
+            if (palindrome < n)
+                return palindrome;
+
+        return -1;
+    }
+
+    private static boolean isPalindrome(int val) {
+        String str = String.valueOf(val);
+
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--)
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int n = in.nextInt();
+            System.out.println(findLargestPalindromeProduct(n));
+        }
+    }
+
+}
