74e4608312f1aa2b6d515ef78e32d2a7863c65ec
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-19 10:11:34 +0300

    Project Euler #3: Largest prime factor

---
+++ Project Euler/#3 - Largest prime factor/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 19.07.2018
+ */
+public class Solution {
+
+    static int largestPrime(long n) {
+        long res = n;
+
+        while (true) {
+            long div = getDiv(res);
+
+            if (div == res)
+                break;
+
+            res /= div;
+        }
+
+        return (int)res;
+    }
+
+    private static long getDiv(long val) {
+        if (val < 2)
+            return val;
+
+        int sqrt = (int)Math.sqrt(val);
+
+        for (int i = 2; i <= sqrt; i++)
+            if (val % i == 0)
+                return i;
+
+        return val;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            long n = in.nextLong();
+            System.out.println(largestPrime(n));
+        }
+    }
+
+}
