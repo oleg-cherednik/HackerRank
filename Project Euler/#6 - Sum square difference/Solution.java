a28cbdcadb23506f4644dbcb59339915733c00a7
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-20 00:40:33 +0300

    Project Euler #6: Sum square difference

---
+++ Project Euler/#6 - Sum square difference/Solution.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 19.07.2018
+ */
+public class Solution {
+
+    private static final Map<Integer, Long> SQUARE = new HashMap<>();
+    private static final Map<Integer, Long> SUM = new HashMap<>();
+
+    static {
+        long sum = 0;
+        long square = 0;
+
+        for (int i = 1; i <= 10000; i++) {
+            sum += i;
+            square += i * i;
+            SQUARE.put(i, square);
+            SUM.put(i, sum * sum);
+        }
+    }
+
+    static long sumSquareDifference(int n) {
+        return Math.abs(SQUARE.get(n) - SUM.get(n));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int n = in.nextInt();
+            System.out.println(sumSquareDifference(n));
+        }
+    }
+
+}
