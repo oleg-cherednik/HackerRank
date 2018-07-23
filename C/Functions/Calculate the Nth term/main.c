ba4e8909c690267748a4954a7d9fd92e3c5067dc
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-23 11:02:26 +0300

    Calculate the Nth term

---
+++ C/Functions/Calculate the Nth term/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * @author Oleg Cherednik
+ * @since 23.07.2018
+ */
+int find_nth_term(int n, int a, int b, int c) {
+    return n == 1 ? a : find_nth_term(n - 1, b, c, a + b + c);
+}
+
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
+    return 0;
+}
+
