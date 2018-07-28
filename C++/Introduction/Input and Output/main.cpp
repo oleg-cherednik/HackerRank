69ad9f9ab417a81c9e86d6b284e78bfe16acaf7a
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-29 01:51:34 +0300

    Input and Output

---
+++ C++/Introduction/Input and Output/main.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+
+    return 0;
+}
