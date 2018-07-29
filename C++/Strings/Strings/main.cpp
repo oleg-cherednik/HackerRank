ea7960acdbf2c53dc04f35bed79c97fd74593e9e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-29 09:57:09 +0300

    Strings

---
+++ C++/Strings/Strings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a = "";
+    string b = "";
+
+    cin >> a >> b;
+
+    cout << a.size() << " " << b.size() << endl;
+    cout << a << b << endl;
+    cout << b[0] << a.substr(1) << " " << a[0] << b.substr(1) << endl;
+
+    return 0;
+}
