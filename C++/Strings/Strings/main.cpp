8578e72cd82d1d9922d80411cc8d21c586f212d4
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-07-29 10:01:36 +0300

    Strings

--- C++/Strings/Strings/main.cpp
+++ C++/Strings/Strings/main.cpp
@@ -8,9 +8,9 @@ int main() {
 
     cin >> a >> b;
 
-    cout << a.size() << " " << b.size() << endl;
+    cout << a.length() << " " << b.length() << endl;
     cout << a << b << endl;
-    cout << b[0] << a.substr(1) << " " << a[0] << b.substr(1) << endl;
+    cout << b.at(0) << a.substr(1) << " " << a.at(0) << b.substr(1) << endl;
 
     return 0;
 }
