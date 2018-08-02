2c068b44afb617685c3ff3175b1e12c34d386467
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-02 09:44:53 +0300

    New Year Chaos

---
+++ Tutorials/Interview Preparation Kit/Arrays/New Year Chaos/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 02.08.2018
+ */
+public class Solution {
+
+    static void minimumBribes(int[] q) {
+        int res = 0;
+
+        for (int i = q.length - 1; i >= 0; i--) {
+            if (q[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+
+            for (int j = Math.max(0, q[i] - 2); j < i; j++)
+                if (q[j] > q[i]) res++;
+        }
+
+        System.out.println(res);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
