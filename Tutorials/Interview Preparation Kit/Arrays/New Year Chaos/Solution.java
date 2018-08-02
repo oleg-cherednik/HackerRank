1d9026f6c7f9510296428574c7f8875f2eccaf6a
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-02 09:46:57 +0300

    New Year Chaos

--- Tutorials/Interview Preparation Kit/Arrays/New Year Chaos/Solution.java
+++ Tutorials/Interview Preparation Kit/Arrays/New Year Chaos/Solution.java
@@ -10,13 +10,13 @@ public class Solution {
         int res = 0;
 
         for (int i = q.length - 1; i >= 0; i--) {
-            if (q[i] - (i + 1) > 2) {
+            if (q[i] - i - 1 > 2) {
                 System.out.println("Too chaotic");
                 return;
             }
 
             for (int j = Math.max(0, q[i] - 2); j < i; j++)
-                if (q[j] > q[i]) res++;
+                res += q[j] > q[i] ? 1 : 0;
         }
 
         System.out.println(res);
