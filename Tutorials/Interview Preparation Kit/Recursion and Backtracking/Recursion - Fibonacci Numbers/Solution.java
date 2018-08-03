52c863ad2c076a34686d0769ac6aa612d9569422
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-04 01:56:35 +0300

    Recursion: Fibonacci Numbers

---
+++ Tutorials/Interview Preparation Kit/Recursion and Backtracking/Recursion - Fibonacci Numbers/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.05.2018
+ */
+public class Solution {
+
+    public static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+
+}
