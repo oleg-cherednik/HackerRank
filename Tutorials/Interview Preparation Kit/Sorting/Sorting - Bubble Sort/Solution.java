d2e0f2480ba4d209981f6d80a1e4f03d9584d6cb
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-04 18:52:06 +0300

    Sorting: Bubble Sort

---
+++ Tutorials/Interview Preparation Kit/Sorting/Sorting - Bubble Sort/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 20.05.2018
+ */
+public class Solution {
+
+    private static int bubbleSore(int[] arr) {
+        int numSwaps = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    numSwaps++;
+                }
+            }
+        }
+
+        return numSwaps;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int n = scan.nextInt();
+            int[] A = new int[n];
+
+            for (int i = 0; i < n; i++)
+                A[i] = scan.nextInt();
+
+            int numSwaps = bubbleSore(A);
+            int firstElement = A[0];
+            int lastElement = A[A.length - 1];
+
+            System.out.println("Array is sorted in " + numSwaps + " swaps.");
+            System.out.println("First Element: " + firstElement);
+            System.out.println("Last Element: " + lastElement);
+        }
+    }
+
+}
