7e2b8cc885b793bd23cd7f8bdab1b48731d5983b
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-05 19:14:11 +0300

    Day 4: Geometric Distribution I

---
+++ Tutorials/10 Days of Statistics/Day 4.3 - Geometric Distribution I/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 05.08.2018
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            double numerator = scan.nextInt();
+            int denominator = scan.nextInt();
+            int n = scan.nextInt();
+            double p = numerator / denominator;
+            System.out.format("%.3f\n", Math.pow(1 - p, n - 1) * p);
+        }
+    }
+}
