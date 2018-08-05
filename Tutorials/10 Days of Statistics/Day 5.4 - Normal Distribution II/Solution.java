d01721c1c540cc149dbb375ab21f3c30b557554f
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-05 20:21:36 +0300

    Day 5: Normal Distribution II

--- Tutorials/10 Days of Statistics/Day 5.4 - Normal Distribution II/Solution.java
+++ Tutorials/10 Days of Statistics/Day 5.4 - Normal Distribution II/Solution.java
@@ -24,14 +24,22 @@ public class Solution {
         return 0.5 * (1 + erf((x - mean) / (1.4142135623730951 * standardDeviation)));
     }
 
-    public static double erf(double z) {
+    /*
+     * Princeton's resources: https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1/forum
+     *
+     * fractional error in math formula less than 1.2 * 10 ^ -7.
+     * although subject to catastrophic cancellation when z in very close to 0
+     * from Chebyshev fitting formula for erf(z) from Numerical Recipes, 6.2
+     */
+    private static double erf(double z) {
         double t = 1.0 / (1.0 + 0.5 * Math.abs(z));
 
         // use Horner's method
         double ans = 1 - t * Math.exp(-z * z - 1.26551223 + t * (1.00002368 + t * (0.37409196 + t * (0.09678418 + t * (-0.18628806
                 + t * (0.27886807 + t * (-1.13520398 + t * (1.48851587 + t * (-0.82215223 + t * 0.17087277)))))))));
 
-        return z >= 0 ? ans : -ans;
+        if (z >= 0) return ans;
+        else return -ans;
     }
 
 }
