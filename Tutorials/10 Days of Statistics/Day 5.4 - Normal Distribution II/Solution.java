c63b9551685648bd89e3c8a85705fd1a7c58ca32
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-05 20:04:51 +0300

    Day 5: Normal Distribution II

--- Tutorials/10 Days of Statistics/Day 5.4 - Normal Distribution II/Solution.java
+++ Tutorials/10 Days of Statistics/Day 5.4 - Normal Distribution II/Solution.java
@@ -7,6 +7,22 @@ import java.util.Scanner;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        double mean = 70;
+        double stdev = 10;
+
+        System.out.println(df.format((1 - normal(80, mean, stdev)) * 100)); //greater than 80
+        System.out.println(df.format((1 - normal(60, mean, stdev)) * 100)); //greater than equal to 60
+        System.out.println(df.format(normal(60, mean, stdev) * 100)); //less than 60
+    }
+
+    public static double normal(double x, double mean, double stdev) {
+        return 0.5 * (1 + erf((x - mean) / (1.4142135623730951 * stdev)));
+    }
+
     public static double erf(double z) {
         double t = 1.0 / (1.0 + 0.5 * Math.abs(z));
 
@@ -23,24 +39,6 @@ public class Solution {
                                                                                 t * (0.17087277))))))))));
         if (z >= 0) return ans;
         else return -ans;
-    }
-
-    public static double normal(double x, double mean, double stdev) {
-        double normal = 0;
-        normal = 0.5 * (1 + erf((x - mean) / (Math.sqrt(2) * stdev)));
-        return normal;
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("###.##");
-
-        double mean = 70;
-        double stdev = 10;
-
-        System.out.println(df.format((1 - normal(80, mean, stdev)) * 100)); //greater than 80
-        System.out.println(df.format((1 - normal(60, mean, stdev)) * 100)); //greater than equal to 60
-        System.out.println(df.format(normal(60, mean, stdev) * 100)); //less than 60
     }
 
 }
