858c2ea7b6221e7ce1ae388fce9490346f686fe7
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-05 21:30:19 +0300

    Day 9: Multiple Linear Regression

--- Tutorials/10 Days of Statistics/Day 9.1 - Multiple Linear Regression/Solution.java
+++ Tutorials/10 Days of Statistics/Day 9.1 - Multiple Linear Regression/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -7,85 +8,42 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        int n = scanner.nextInt();
-        double[] Y = new double[n];
-        double[][] X = new double[n][m + 1];
-        for (int i = 0; i < n; i++) {
-            X[i][0] = 1;
-            for (int j = 0; j < m; j++) {
-                X[i][j + 1] = scanner.nextDouble();
-            }
-            Y[i] = scanner.nextDouble();
-        }
-        double[] b = solve(mult(transpose(X), X), mult(transpose(X), Y), 1e-8);
-        ///
-        int q = scanner.nextInt();
-        double[] obs = new double[m + 1];
-        double tmp = 0;
-        for (int i = 0; i < q; i++) {
-            obs[0] = 1;
-            for (int j = 0; j < m; j++) {
-                obs[j + 1] = scanner.nextDouble();
-            }
-            tmp = 0;
-            for (int j = 0; j < m + 1; j++) {
-                tmp += b[j] * obs[j];
-            }
-            System.out.format("%.9f\n", tmp);
-        }
-    }
+        try (Scanner scan = new Scanner(System.in)) {
+            scan.useLocale(Locale.US);
 
-    public static double[][] transpose(double[][] X) {
-        int n = X.length;
-        int m = X[0].length;
-        double[][] XT = new double[m][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                XT[j][i] = X[i][j];
-            }
-        }
-        return XT;
-    }
+            int m = scan.nextInt();
+            int n = scan.nextInt();
+            double[] Y = new double[n];
+            double[][] X = new double[n][m + 1];
 
-    /**
-     * Matrix times matrix
-     */
-    public static double[][] mult(double[][] X, double[][] Y) {
-        int n = X.length;
-        int m = X[0].length;
-        int mCheck = Y.length;
-        int p = Y[0].length;
-        double[][] A = new double[n][p];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < p; j++) {
-                double tmp = 0;
-                for (int k = 0; k < m; k++) {
-                    tmp += X[i][k] * Y[k][j];
-                }
-                A[i][j] = tmp;
-            }
-        }
-        return A;
-    }
+            for (int i = 0; i < n; i++) {
+                X[i][0] = 1;
 
-    /**
-     * Matrix times vector
-     */
-    public static double[] mult(double[][] X, double[] Y) {
-        int n = X.length;
-        int m = X[0].length;
-        int p = Y.length;
-        double[] z = new double[n];
-        for (int i = 0; i < n; i++) {
-            double tmp = 0;
-            for (int k = 0; k < p; k++) {
-                tmp += X[i][k] * Y[k];
+                for (int j = 0; j < m; j++)
+                    X[i][j + 1] = scan.nextDouble();
+
+                Y[i] = scan.nextDouble();
             }
-            z[i] = tmp;
+
+            double[] b = solve(mult(transpose(X), X), mult(transpose(X), Y), 1e-8);
+
+            int q = scan.nextInt();
+            double[] obs = new double[m + 1];
+
+            for (int i = 0; i < q; i++) {
+                obs[0] = 1;
+
+                for (int j = 0; j < m; j++)
+                    obs[j + 1] = scan.nextDouble();
+
+                double res = 0;
+
+                for (int j = 0; j < m + 1; j++)
+                    res += b[j] * obs[j];
+
+                System.out.format(Locale.US, "%.9f\n", res);
+            }
         }
-        return (z);
     }
 
     private static double[] solve(double[][] A, double y[], double tol) {
@@ -111,6 +69,49 @@ public class Solution {
 
             if (epsilon < tol)
                 break;
+        }
+
+        return z;
+    }
+
+    private static double[][] transpose(double[][] X) {
+        double[][] xt = new double[X[0].length][X.length];
+
+        for (int i = 0; i < X.length; i++)
+            for (int j = 0; j < X[0].length; j++)
+                xt[j][i] = X[i][j];
+
+        return xt;
+    }
+
+    private static double[][] mult(double[][] X, double[][] Y) {
+        int p = Y[0].length;
+        double[][] A = new double[X.length][p];
+
+        for (int i = 0; i < X.length; i++) {
+            for (int j = 0; j < p; j++) {
+                double tmp = 0;
+
+                for (int k = 0; k < X[0].length; k++)
+                    tmp += X[i][k] * Y[k][j];
+
+                A[i][j] = tmp;
+            }
+        }
+
+        return A;
+    }
+
+    private static double[] mult(double[][] X, double[] Y) {
+        double[] z = new double[X.length];
+
+        for (int i = 0; i < X.length; i++) {
+            double tmp = 0;
+
+            for (int k = 0; k < Y.length; k++)
+                tmp += X[i][k] * Y[k];
+
+            z[i] = tmp;
         }
 
         return z;
