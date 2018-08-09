9f8fbb32495f2ee9065cb8f8b0a69cb7e4961f46
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-09 22:43:01 +0300

    Project Euler #12: Highly divisible triangular number

---
+++ Compete/Project Euler/#012 - Highly divisible triangular number/Solution.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 09.08.2018
+ */
+public class Solution {
+
+    static long powerDigitSum(int n) {
+        String str = BigInteger.valueOf(2).pow(n).toString();
+
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++)
+            sum += str.charAt(i) - '0';
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int n = in.nextInt();
+            System.out.println(powerDigitSum(n));
+        }
+    }
+
+}
