866929a81f72447658b6897e73c6e5f98138076e
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-10 14:08:26 +0300

    Project Euler #25: N-digit Fibonacci number

--- Compete/Project Euler/#025 - N-digit Fibonacci number/Solution.java
+++ Compete/Project Euler/#025 - N-digit Fibonacci number/Solution.java
@@ -6,18 +6,39 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    private static final double GOLDEN_RATION_PHI = 1.61803;
+    private static int numberOfDigits(int n) {
+        if (n == 1)
+            return 1;
 
-    static long findFibonacci(int N) {
-        return (long)Math.ceil((Math.log(10) * (N - 1) + Math.log(5) / 2) / Math.log(GOLDEN_RATION_PHI));
+        double ans = (n * Math.log10(1.6180339887498948)) - ((Math.log10(5)) / 2);
+        return (int)Math.ceil(ans);
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         try (Scanner scan = new Scanner(System.in)) {
             int T = scan.nextInt();
 
-            for (int i = 0; i < T; i++)
-                System.out.println(findFibonacci(scan.nextInt()));
+            for (int j = 0; j < T; j++) {
+                int n = scan.nextInt();
+                int low = 0;
+                int high = 25000;
+                int mid = 0, mid_n = 0;
+
+                while (low < high) {
+                    mid = (low + high) / 2;
+                    mid_n = numberOfDigits(mid);
+
+                    if (mid_n >= n)
+                        high = mid;
+                    else
+                        low = mid + 1;
+
+                }
+
+
+                System.out.println((low + high) / 2);
+            }
         }
     }
+
 }
