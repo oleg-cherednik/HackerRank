5e1ae31d8ed6d1a25d25b5e51746751016785fb0
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-08-15 09:23:11 +0300

    Project Euler #12: Highly divisible triangular number

--- Compete/Project Euler/#012 - Highly divisible triangular number/Solution.java
+++ Compete/Project Euler/#012 - Highly divisible triangular number/Solution.java
@@ -1,4 +1,4 @@
-import java.math.BigInteger;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 /**
@@ -7,24 +7,57 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    static long powerDigitSum(int n) {
-        String str = BigInteger.valueOf(2).pow(n).toString();
-
-        int sum = 0;
-
-        for (int i = 0; i < str.length(); i++)
-            sum += str.charAt(i) - '0';
-
-        return sum;
-    }
+    private static Hashtable<Integer, Integer> dict;
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int a0 = 0; a0 < t; a0++) {
-            int n = in.nextInt();
-            System.out.println(powerDigitSum(n));
+        Scanner sc = new Scanner(System.in);
+        dict = new Hashtable<>();
+        int tc = sc.nextInt();
+        for (int i_tc = 0; i_tc < tc; i_tc++) {
+            int n = sc.nextInt();
+            int number = 1;
+            int triangle = getTriangle(number);
+            int divisorCount = numberOfDivisors(triangle);
+            dict.put(number, divisorCount);
+            while (divisorCount <= n) {
+                number += 1;
+                triangle = getTriangle(number);
+                divisorCount = numberOfDivisors(triangle);
+                dict.put(triangle, divisorCount);
+            }
+            System.out.println(triangle);
         }
+    }
+
+    private static int numberOfDivisors(int triangle) {
+        if (dict.containsKey(triangle)) {
+            return dict.get(triangle);
+        } else {
+            int tempTriangle = triangle;
+            int primePowerCount = 1, count = 0;
+            while (triangle % 2 == 0) {
+                triangle = Math.floorDiv(triangle, 2);
+                count += 1;
+            }
+            primePowerCount *= (count + 1);
+            for (int i = 3; i <= Math.sqrt(triangle); i++) {
+                count = 0;
+                while (triangle % i == 0) {
+                    count += 1;
+                    triangle = Math.floorDiv(triangle, i);
+                }
+                primePowerCount *= (count + 1);
+            }
+            if (triangle > 2) {
+                primePowerCount *= 2;
+            }
+            dict.put(tempTriangle, primePowerCount);
+            return dict.get(tempTriangle);
+        }
+    }
+
+    private static int getTriangle(int number) {
+        return Math.floorDiv(number * (number + 1), 2);
     }
 
 }
