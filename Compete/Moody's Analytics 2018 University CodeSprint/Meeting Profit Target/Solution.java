b4f3fa43651d2b01e901835a7e90d71e4f6c1408
Oleg Cherednik <oleg.cherednik@xyleme.com>
2018-08-18 13:48:48 +0300

    Meeting Profit Target

--- Compete/Moody's Analytics 2018 University CodeSprint/Meeting Profit Target/Solution.java
+++ Compete/Moody's Analytics 2018 University CodeSprint/Meeting Profit Target/Solution.java
@@ -1,51 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * @author Oleg Cherednik
  * @since 18.08.2018
  */
 public class Solution {
-    static int solve(List<List<Integer>> profits) {
+    static int solve(int n, int[] actual, int[] estimated) {
         long sum = 0;
 
-        for (List<Integer> profit : profits) {
-            int actual = profit.get(0);
-            int estimated = profit.get(1);
-            int delta = actual - estimated;
-
-            sum = Math.min(0, sum + delta);
-        }
+        for (int i = 0; i < n; i++)
+            sum = Math.min(0, sum + actual[i] - estimated[i]);
 
         return sum < 0 ? 1 : 0;
     }
 
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
-            int t = Integer.parseInt(scan.nextLine().trim());
+            int q = scan.nextInt();
 
-            IntStream.range(0, t).forEach(tItr -> {
-                int n = Integer.parseInt(scan.nextLine().trim());
+            for (int i = 0; i < q; i++) {
+                int n = scan.nextInt();
+                int[] actual = new int[n];
+                int[] estimated = new int[n];
 
-                List<List<Integer>> profits = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    actual[j] = scan.nextInt();
+                    estimated[j] = scan.nextInt();
+                }
 
-                IntStream.range(0, n).forEach(i -> {
-                    profits.add(
-                            Stream.of(scan.nextLine().replaceAll("\\s+$", "").split(" "))
-                                  .map(Integer::parseInt)
-                                  .collect(toList())
-                    );
-                });
-
-                int res = solve(profits);
-
-                System.out.println(String.valueOf(res));
-            });
+                System.out.println(solve(n, actual, estimated));
+            }
         }
     }
 
