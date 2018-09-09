84c56fc7c1f4e71869eddee5c9275b157078d61d
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-09-10 02:24:30 +0300

    Project Euler #21: Amicable numbers

--- Compete/Project Euler/#021 - Amicable numbers/Solution.java
+++ Compete/Project Euler/#021 - Amicable numbers/Solution.java
@@ -1,43 +1,50 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author Oleg Cherednik
- * @since 09.09.2018
+ * @since 10.09.2018
  */
 public class Solution {
 
-    private static final Map<Integer, Integer> MAP = new HashMap<>();
+    private static final Set<Integer> AMICABLE_NUMBERS;
 
-    static int findAmicableSum(int N) {
-        int res = 0;
+    static {
+        Set<Integer> amicableNumbers = new TreeSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int a = 1; a <= N; a++) {
-            MAP.computeIfAbsent(a, Solution::getDivisorsSum);
-            int b = MAP.get(a);
+        for (int a = 1; a <= 100_000; a++) {
+            map.computeIfAbsent(a, Solution::getDivisorsSum);
+            map.computeIfAbsent(map.get(a), Solution::getDivisorsSum);
 
-            MAP.computeIfAbsent(b, Solution::getDivisorsSum);
-            int c = MAP.get(b);
+            int b = map.get(a);
+            int c = map.get(b);
 
-            if (a >= b || a != c)
-                continue;
-
-            res += a < N ? a : 0;
-            res += b < N ? b : 0;
+            if (a < b && a == c) {
+                amicableNumbers.add(a);
+                amicableNumbers.add(b);
+            }
         }
 
-        return res;
+        AMICABLE_NUMBERS = Collections.unmodifiableSet(amicableNumbers);
+    }
+
+    static int findAmicableSum(int N) {
+        return AMICABLE_NUMBERS.stream().filter(v -> v < N).mapToInt(v -> v).sum();
     }
 
     private static int getDivisorsSum(int val) {
         int sum = 0;
 
-        for (int i = 1, sqrt = (int)Math.sqrt(val); i <= val / 2; i++) {
-            if (i == sqrt && sum == 1)
-                break;
-            if (val % i == 0)
+        for (int i = 1, sqrt = (int)Math.sqrt(val); i <= sqrt; i++) {
+            if (val % i == 0) {
                 sum += i;
+                sum += i > 1 ? val / i : 0;
+            }
         }
 
         return sum;
