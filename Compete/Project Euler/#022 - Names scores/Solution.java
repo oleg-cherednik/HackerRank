a60a291e6d0b98553d4f200ef5b9a7dabfbefb50
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-09-10 09:42:31 +0300

    Project Euler #22: Names scores

---
+++ Compete/Project Euler/#022 - Names scores/Solution.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Oleg Cherednik
+ * @since 10.09.2018
+ */
+public class Solution {
+
+    static int getNameScore(String name, Map<String, Integer> names) {
+        int res = 0;
+
+        for (int i = 0; i < name.length(); i++)
+            res += name.charAt(i) - 'a' + 1;
+
+        return res * names.get(name);
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int N = scan.nextInt();
+
+            Set<String> names = new TreeSet<>();
+
+            for (int i = 0; i < N; i++)
+                names.add(scan.next().toLowerCase());
+
+            int i = 1;
+            Map<String, Integer> map = new HashMap<>();
+
+            for (String name : names)
+                map.put(name, i++);
+
+            int Q = scan.nextInt();
+
+            for (int j = 0; j < Q; i++)
+                System.out.println(getNameScore(scan.next().toLowerCase(), map));
+        }
+    }
+}
