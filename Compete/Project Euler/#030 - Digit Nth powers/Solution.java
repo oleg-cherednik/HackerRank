dd8311dc915082f99dddfa1fe7c37dfdda31154e
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-06-17 20:08:01 +0300

    Project Euler #30: Digit Nth powers

---
+++ Compete/Project Euler/#030 - Digit Nth powers/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * @author Oleg Cherednik
+ * @since 17.06.2020
+ */
+public class Solution {
+
+    private static int calc(int n) {
+        int res = 0;
+
+        for (int i = 10; i < 600_000; i++) {
+            int sum = 0;
+            int j = i;
+
+            while (j > 0) {
+                int d = j % 10;
+                sum += Math.pow(d, n);
+                j /= 10;
+            }
+
+            if (sum == i)
+                res += i;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println(calc(scan.nextInt()));
+        }
+    }
+
+}
