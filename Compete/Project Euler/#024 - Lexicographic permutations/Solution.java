801d428182f4c50dd1c71670fe7e0f2286086d2a
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-06-21 14:56:46 +0300

    Project Euler #24: Lexicographic permutations

--- Compete/Project Euler/#024 - Lexicographic permutations/Solution.java
+++ Compete/Project Euler/#024 - Lexicographic permutations/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -6,38 +9,31 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    private static final String STR = "abcdefghijklm";
-    private static final long[] FACT = { 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600 };
+    private static String lexicographicPermutation(String str, int n) {
+        long[] fact = new long[str.length()];
+        List<Character> letters = new ArrayList<>(str.length());
 
-    private static String lexicographicPermutation(long n) {
+        for (int i = 0; i < str.length(); fact[i] = i == 0 ? 1 : i * fact[i - 1], i++)
+            letters.add(str.charAt(i));
+
+        letters.sort(Comparator.naturalOrder());
+
         n--;
-        char[] arr = STR.toCharArray();
+        StringBuilder buf = new StringBuilder(str.length());
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            long fact = FACT[arr.length - i - 2];
-            long p = i + n / fact;
+        for (int i = str.length() - 1; i >= 0; n %= fact[i], i--)
+            buf.append(letters.remove((int)(n / fact[i])));
 
-            n %= fact;
-
-            for (int j = i + 1; j <= p; j++)
-                swap(arr, i, j);
-        }
-
-        return new String(arr);
-    }
-
-    private static void swap(char[] arr, int i, int j) {
-        char tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        return buf.toString();
     }
 
     public static void main(String... args) {
         try (Scanner scan = new Scanner(System.in)) {
             int T = scan.nextInt();
+            final String str = "abcdefghijklm";
 
             for (int i = 0; i < T; i++)
-                System.out.println(lexicographicPermutation(scan.nextLong()));
+                System.out.println(lexicographicPermutation(str, scan.nextInt()));
         }
     }
 
