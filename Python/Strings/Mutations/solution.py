da7e2f1f6e714ec06fca430cb5f573c5a337c865
Oleg Cherednik <oleg_cherednik@epam.com>
2020-08-09 10:51:25 +0300

    Python: Mutations

---
+++ Python/Strings/Mutations/solution.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
