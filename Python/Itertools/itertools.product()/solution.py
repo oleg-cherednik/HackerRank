4dbd3ac0102c28177a329a8b3109f593eedf3ca0
Oleg Cherednik <oleg_cherednik@epam.com>
2020-08-16 14:15:53 +0300

    Python: itertools.product()

--- Python/Itertools/itertools.product()/solution.py
+++ Python/Itertools/itertools.product()/solution.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 
-import itertools
+from itertools import product
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    print(*itertools.product(A, B))
+    print(*product(A, B))
