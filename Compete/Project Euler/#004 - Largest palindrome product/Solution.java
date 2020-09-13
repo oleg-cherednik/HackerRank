5628c5c5be25d5a77c1a50f1b0e7f54fdfa72220
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-09-13 23:48:15 +0300

    Project Euler #4: Largest palindrome product

--- Compete/Project Euler/#004 - Largest palindrome product/Solution.java
+++ Compete/Project Euler/#004 - Largest palindrome product/Solution.java
@@ -45,4 +45,9 @@ public class Solution {
         }
     }
 
+//    public static void main(String... args) {
+//        System.out.println(PALINDROMES.iterator().next());  // 906609
+//    }
+
+
 }
