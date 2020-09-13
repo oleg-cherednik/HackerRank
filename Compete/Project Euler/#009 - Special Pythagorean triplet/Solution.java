d451282eb02511e661ae465e045103d153dc96f8
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-09-13 23:59:12 +0300

    Project Euler #9: Special Pythagorean triplet

--- Compete/Project Euler/#009 - Special Pythagorean triplet/Solution.java
+++ Compete/Project Euler/#009 - Special Pythagorean triplet/Solution.java
@@ -29,4 +29,8 @@ public class Solution {
         }
     }
 
+//    public static void main(String... args) {
+//        System.out.println(findMaxPythagoreanTriplet(1000));    // 31875000
+//    }
+
 }
