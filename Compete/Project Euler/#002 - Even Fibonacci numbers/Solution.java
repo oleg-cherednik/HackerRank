2f77eb7b18535ad081adc89e073201d9640f9d04
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-09-13 23:04:06 +0300

    Project Euler #2: Even Fibonacci numbers

--- Compete/Project Euler/#002 - Even Fibonacci numbers/Solution.java
+++ Compete/Project Euler/#002 - Even Fibonacci numbers/Solution.java
@@ -43,4 +43,8 @@ public class Solution {
         }
     }
 
+//    public static void main(String... args) {
+//        System.out.println(findEvenFibonacciSum(4_000_000));    //  4613732
+//    }
+
 }
