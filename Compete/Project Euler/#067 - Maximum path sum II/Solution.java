30a0c136a316a6f225c5969681aed0e6902604c8
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-09-19 17:18:07 +0300

    Project Euler #67: Maximum path sum II

--- Compete/Project Euler/#067 - Maximum path sum II/Solution.java
+++ Compete/Project Euler/#067 - Maximum path sum II/Solution.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    private static int findMaxSum(int[][] data) {
+    public static int findMaxSum(int[][] data) {
         for (int row = data.length - 2; row >= 0; row--)
             for (int col = 0; col < data[row].length; col++)
                 data[row][col] += Math.max(data[row + 1][col], data[row + 1][col + 1]);
